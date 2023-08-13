eeecf12e457ac15f7fb8ad4f556f51ddad8b6bf5
Arshdeep <arshdeepsinghbhatia3@gmail.com>
2023-08-13 06:12:43 +0530

    initial commit

---
+++ quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-dark">
+        <q-toolbar-title class="q-pa-md">
+          Anime Tracker
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+// import EssentialLink from 'components/EssentialLink.vue'
+
+
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+
+  },
+
+  setup () {
+
+
+    return {
+
+   
+    }
+  }
+})
+</script>
---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="q-pa-md ">
+    <div class="row justify-center">
+      <div>
+        <q-card flat>
+          <div class="row">
+            <q-card-section class="row">
+              <q-input class="col" v-model="search_query" outlined style="width:450px"
+                label="search any anime..."></q-input> <q-btn icon="send" color="black"
+                @click="submitInput(search_query)"></q-btn>
+            </q-card-section>
+          </div>
+
+
+
+        </q-card>
+      </div>
+    </div>
+    <div class="row justify-center q-ma-xl" v-if="data">
+      <q-card style="width:500px">
+        
+        <q-card-section>
+          <q-card-title class="text-bold" style="font-size: x-large;">
+            {{ result['title'] }}
+            <hr><br>
+          </q-card-title>
+          <q-img :src="result['img-url']" :ratio="16 / 9" />
+          <p class="q-pt-md"><b>Storyline:</b><br>{{ result['description'] }}</p>
+
+        </q-card-section>
+        <q-table class="q-pa-md" :hide-pagination="true" title="More Info" :columns="columns" :rows="rows" flat></q-table>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import { api } from 'src/boot/axios'
+
+
+
+
+export default {
+  name: 'IndexPage',
+  setup() {
+
+    const search_query = ref(null)
+    const data = ref(null)
+    const result = ref({})
+    // const columns = [
+    //   { label: 'Release Date', name: 'ReleaseDate' },
+    //   { label: 'Status', name: 'Status' },
+    //   { label: 'Total Episodes', name: 'TotalEpisodes' },
+    //   { label: 'episode length(mins)', name: 'episodeLength' },
+    // ]
+    const rows = ref([])
+    const columns = [
+      {
+        name: 'releaseDate',
+        field: 'releaseDate',
+        label: 'Release Date',
+      },
+      {
+        name: 'Status',
+        field: 'Status',
+        label: 'Status',
+      },
+      {
+        name: 'TotalEpisodes',
+        field: 'TotalEpisodes',
+        label: 'Total Episodes',
+      },
+      {
+        name: 'episodeLength',
+        field: 'episodeLength',
+        label: 'Episode length(mins)',
+      },
+      
+
+    ]
+    
+
+
+
+    async function submitInput(search_query) {
+      const response = await api.get(search_query)
+      data.value = response.data
+      console.log(data.value)
+      result.value['title'] = data.value.data[0].attributes.titles.en
+      result.value['description'] = data.value.data[0].attributes.synopsis
+      result.value['img-url'] = data.value.data[0].attributes.coverImage.original
+      result.value['ep-count'] = data.value.data[0].attributes.episodeCount
+      result.value['ep-size'] = data.value.data[0].attributes.episodeLength
+      result.value['release'] = data.value.data[0].attributes.startDate
+      result.value['status'] = data.value.data[0].attributes.status
+
+      rows.value = [
+        {
+          releaseDate: result.value['release'], Status: result.value['status'], TotalEpisodes: result.value['ep-count'], episodeLength: result.value['ep-size']
+        },
+      ]
+    }
+
+
+    return {
+      search_query, submitInput, data, result, columns, rows
+    }
+  }
+
+
+}
+
+
+</script>
